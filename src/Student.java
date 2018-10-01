6f39477049a55571a623a9ef018fcc92cc7af71a
Frank Enciso <fenciso@mines.edu>
2018-10-01 17:44:11 -0600

    Added JavaDoc to files

--- src/Student.java
+++ src/Student.java
@@ -6,26 +6,34 @@
 public class Student implements Comparable<Student> {
 	private String name;
 	private int score;
-	
+	/**
+	 * Parameterized constructor that sets the private attributes
+	 * @param name - name of the student
+	 * @param score - the student's score
+	 */
 	public Student(String name, int score) {
 		this.name = name;
 		this.score = score;
 	}
-	
-	
-
+	/**
+	 * Getter for Student name
+	 * @return name - gets the name
+	 */
 	public String getName() {
 		return name;
 	}
-
-
-
+	/**
+	 * Converts the data to a user friendly output string
+	 * @return - returns the easy to read string
+	 */
 	@Override
 	public String toString() {
 		return (name + " " + score);
 	}
-
-
+	/**
+	 * The Comparable<Student> interface requires creating a compareTo function
+	 * @return - returns the comparison result for 2 students
+	 */
 	@Override
 	public int compareTo(Student other) {
 		return (this.name).compareTo(other.name);
